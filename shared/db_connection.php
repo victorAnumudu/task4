79bab9e1ef2fdefeb5ab4563f00b4f04620fe318
= <=>
2021-07-18 21:21:36 +0100

    added hosted linked to reademe file

--- shared/db_connection.php
+++ shared/db_connection.php
@@ -1,19 +1,19 @@
 
 <?php 
 
-// $localserver = "localhost";
-// $db_user = "root";
-// $db_password = "";
-// $db_name = "sales";
-
-// $connection = new mysqli($localserver, $db_user, $db_password, $db_name);
-
-// connecting to freemysqlhosting.net
-$localserver = "sql4.freemysqlhosting.net";
-$db_user = "sql4426086";
-$db_password = "usQkfvunaZ";
-$db_name = "sql4426086";
+$localserver = "localhost";
+$db_user = "root";
+$db_password = "";
+$db_name = "sales";
 
 $connection = new mysqli($localserver, $db_user, $db_password, $db_name);
+
+// connecting to freemysqlhosting.net
+// $localserver = "sql4.freemysqlhosting.net";
+// $db_user = "sql4426086";
+// $db_password = "usQkfvunaZ";
+// $db_name = "sql4426086";
+
+// $connection = new mysqli($localserver, $db_user, $db_password, $db_name);
 
 ?>
---
+++ sql/readme.md
@@ -0,0 +1,1 @@
+https://sales001.herokuapp.com/
